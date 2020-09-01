60eb4189e613118c6a479fa2c164893584ee816d
Sumida82 <69194257+Sumida82@users.noreply.github.com>
2020-09-01 16:46:42 +0900

    userモデルのバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        has_many :items
+        has_many :purchases
+
+        with_options presence: true do
+          validates :name
+          with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+            validates :first_name
+            validates :family_name
+          end
+          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+            validates :first_name_kana
+            validates :family_name_kana
+          end
+          validates :birth
+        end
+
 end
