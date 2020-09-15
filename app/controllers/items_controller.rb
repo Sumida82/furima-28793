aa1ad153e37c2ee9b1a54eafdaed9d3402d99a09
Sumida82 <69194257+Sumida82@users.noreply.github.com>
2020-09-15 12:36:32 +0900

    商品詳細を表示する機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def new
     @item = Item.new
@@ -16,6 +16,11 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
+
   end
 
   private
